b9fcdc85e85d28e2bd67d609fa5826f0aa3b9124
Nataly Baranova <borashek@inbox.ru>
2024-03-26 20:23:42 +0300

    feat: 123

--- router/index.js
+++ router/index.js
@@ -5,14 +5,8 @@ const AuthMiddleware = require('./../middleware/AuthMiddleware')
 const { body } = require('express-validator')
 const jwt = require('jsonwebtoken')
 const ApiError = require('../exceptions/ApiError')
-const allowCors = require('../service/CorsService')
 
 const router = new Router()
-
-const handler = (req, res) => {
-  const d = new Date()
-  res.end(d.toString())
-}
 
 const authenticateToken = (req, res, next) => {
   const token = req.header('Authorization')
@@ -43,28 +37,28 @@ router.post(
       .isLength({ min: 4, max: 164 })
       .withMessage('Password must be between 4 and 164 characters.'),
   ],
-  allowCors, AuthController.registration
+  AuthController.registration
 )
 
 // kinda ready
 // auth
-router.post('/login', allowCors, AuthController.login) // add remember me 
-router.get('/verify/:verificationLink', allowCors, AuthController.verify)
-router.post('/forgot-password', allowCors, AuthController.forgotPassword)
-router.get('/create-new-password/:createNewPasswordLink/:email', allowCors, AuthController.createNewPassword)
-router.post('/save-new-password', allowCors, AuthController.saveNewPassword)
+router.post('/login', AuthController.login) // add remember me 
+router.get('/verify/:verificationLink', AuthController.verify)
+router.post('/forgot-password', AuthController.forgotPassword)
+router.get('/create-new-password/:createNewPasswordLink/:email', AuthController.createNewPassword)
+router.post('/save-new-password', AuthController.saveNewPassword)
 
-router.post('/change-password', AuthMiddleware, allowCors, AuthController.changePassword)
-router.post('/change-email', AuthMiddleware, allowCors, AuthController.changeEmail)
-router.get('/me', AuthMiddleware, allowCors, AuthController.me)
-router.post('/logout', AuthMiddleware, allowCors, AuthController.logout)
+router.post('/change-password', AuthMiddleware, AuthController.changePassword)
+router.post('/change-email', AuthMiddleware, AuthController.changeEmail)
+router.get('/me', AuthMiddleware, AuthController.me)
+router.post('/logout', AuthMiddleware, AuthController.logout)
 
-router.get('/refresh', allowCors, AuthController.refresh)
+router.get('/refresh', AuthController.refresh)
 
 // users - score
-router.get('/get-total-user-score/:userId', AuthMiddleware, allowCors, UserController.getTotalUserScore)
-router.post('/update-user-score', AuthMiddleware, allowCors, UserController.updateUserScore)
-router.post('/update-user-avatar', AuthMiddleware, allowCors, UserController.updateUserAvatar)
+router.get('/get-total-user-score/:userId', AuthMiddleware, UserController.getTotalUserScore)
+router.post('/update-user-score', AuthMiddleware, UserController.updateUserScore)
+router.post('/update-user-avatar', AuthMiddleware, UserController.updateUserAvatar)
 // to do
 
 // to check
