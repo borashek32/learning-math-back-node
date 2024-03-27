1350487b7844231d745970871f2e5a56843a1730
Nataly Baranova <borashek@inbox.ru>
2024-03-27 13:37:50 +0300

    feat: change expiration time of access token

--- service/TokenService.js
+++ service/TokenService.js
@@ -7,7 +7,7 @@ class TokenService {
   }
 
   generateTokens(payload) {
-    const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_TOKEN, { expiresIn: '3s' })
+    const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_TOKEN, { expiresIn: '30m' })
     const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_TOKEN, { expiresIn: '10d' })
 
     return { accessToken, refreshToken }
