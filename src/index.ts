1837aba5cd25daccbc28cf2588e77bab4500f73b
Nataly Baranova <borashek@inbox.ru>
2024-08-07 16:56:46 +0300

    feat: change

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ dotenv.config();
 const PORT = 7001;
 
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: "http://localhost:3000" || "https://learning-math-front-react.vercel.app",
   credentials: true,
 };
 
